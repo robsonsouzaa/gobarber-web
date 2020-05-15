34e7e229a5fd390cae53baf1c6b8b4109a9514ae
souzaex <robsonex@gmail.com>
2020-05-15 16:03:09 -0400

    página de cadastro

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signUpBackground from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh; /* sempre assumir 100% da tela de visão que o usuário tem, total da altura */
+
+  display: flex;
+  align-items: stretch; /* os itens dentro do container tb terão o valor total da tela */
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center; /* coloca os elementos centralizados */
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 24px;
+  }
+
+  a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+  }
+
+  /* vai pegar apenas os links que estão dentro do Content, não entra nos sub-níveis,
+  dentro do <form> por exemplo */
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 15px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1; /* ocupar todo o espaço, menos os 700px */
+  background: url(${signUpBackground}) no-repeat center;
+  background-size: cover; /* garantir que a imagem vai preencher todo o conteúdo */
+`;
