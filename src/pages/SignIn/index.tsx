67aa083e38f110e5fae5e240b5f3fa6eab059b66
Robson Souza <robsonex@gmail.com>
2020-06-09 21:34:40 -0400

    feat: adicionado api de contexto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -11,10 +11,14 @@ import Button from '../../components/Button';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
+import Authcontext from '../../context/AuthContext';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(Authcontext);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
